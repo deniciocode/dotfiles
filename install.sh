5d2e68e4511fc0b0d8f2489a8148c841b06b6218
Dennis Haegler <dennis.haegler@gmail.com>
2024-06-07 17:52:10 +0200

    Add install script

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+echo "Hello World"
+
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore ~/.gitignore
 ln -s ~/dotfiles/zshrc ~/.zshrc
+
+# mkdir ~/Library/Application\ Support/iTerm2/Scripts/AutoLaunch
+# cp ./auto_switch_theme.py ~/Library/Application\ Support/iTerm2/Scripts/AutoLaunch/auto_switch_theme.py
